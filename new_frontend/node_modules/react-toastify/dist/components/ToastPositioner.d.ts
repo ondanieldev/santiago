6b1df1262808bd50c85b61fd105e52c53df5cd56
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2020-11-23 10:38:02 -0300

    revert(global): removing duplicated folders :rocket:

--- new_frontend/node_modules/react-toastify/dist/components/ToastPositioner.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import { ToastProps } from '../types';
-declare type ToastPositionerProps = Pick<ToastProps, 'className' | 'style' | 'in'>;
-export declare const ToastPositioner: React.FC<ToastPositionerProps>;
-export {};
